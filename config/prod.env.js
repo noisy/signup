0db1d2f41647ec8f56a85cfa4c913c01827eefff
Ícaro Harry <me@icaro.io>
2018-05-31 17:04:19 -0300

    Implement option to connect existing Steem account

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,8 @@ module.exports = {
   ROOT_PATH: '"https://signup.utopian.io"',
   API_PATH: '"https://api.utopian.io"',
   FACEBOOK_CLIENT_ID: '""', // CHANGE THIS
-  GITHUB_CLIENT_ID: '"dfd32cd4cc6b9272a126"', // CHANGE THIS
+  GITHUB_CLIENT_ID: '"dfd32cd4cc6b9272a126"', // CHANGE THIS,
+  RECAPTCHA_KEY: '"6LemTFoUAAAAALXAe6O-HyArFBFI6AqMScBkBkid"',
+  STEEM_CONNECT_ID: '"utopian"',
   LINKEDIN_CLIENT_ID: '""' // CHANGE THIS
 }
---
+++ src/components/steps/connect/connect_account.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="Box__container">
+    <div class="Box__inner">
+      <loadingbar class="loadingbar"/>
+      <h1>Welcome to Utopian.io!</h1>
+      <p>Please select the option to proceed with the account creation.</p>
+      <div class="options">
+        <button class="Btn__blue" @click="noAccount()">I DON'T HAVE AN STEEM ACCOUNT</button>
+        <button class="Btn__blue" @click="steemConnect()">CONNECT MY EXISTING STEEM ACCOUNT</button>
+      </div>
+    </div>
+  <Login/>
+  </div>
+</template>
+
+<script>
+import loadingbar from './../../partials/loading_bars/loading_bar_1_3.vue'
+import * as SteemConnect from 'sc2-sdk'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    loadingbar,
+  },
+  computed: {
+    ...mapGetters({ user: 'currentUserObject' }),
+  },
+  methods: {
+    noAccount() {
+      if(!this.user.social_verified && !this.user.sms_verified && !this.user.email_verified) { this.$router.push('/verify_invite') }
+      else if((this.user.social_verified || this.user.sms_verified) && this.user.email_verified) { this.$router.push('/pick_account') }
+      else if(!this.user.email_verified) { this.$router.push('/verify_mail') }
+      else { this.$router.push('/verify_phone') }
+    },
+    steemConnect() {
+      const sc2 = SteemConnect.Initialize({
+        app: process.env.STEEM_CONNECT_ID,
+        callbackURL: `${process.env.ROOT_PATH}/success_connect`,
+        scope: ['login']
+      });
+      location.href = sc2.getLoginURL(this.user._id)
+    }
+  }
+}
+</script>
+
+<style>
+
+.options {
+  display: flex;
+  flex-direction: column;
+}
+
+.Btn__blue {
+  margin-top: 10px;
+  width: 100%;
+}
+
+.Box__key {
+  height: 55.44px;
+  width: 336px;
+  border-left: 1px solid #E0E2E5;
+}
+
+.loadingbar {
+  margin-top:12px !important;
+  margin-bottom:12px !important;
+}
+
+</style>
