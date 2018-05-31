14cee67b68289f0bb3444726904e0f72757e0e8a
Diego Pucci <me@diegopucci.com>
2018-05-31 22:47:37 +0200

    sc client id

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,6 +6,6 @@ module.exports = {
   FACEBOOK_CLIENT_ID: '""', // CHANGE THIS
   GITHUB_CLIENT_ID: '"dfd32cd4cc6b9272a126"', // CHANGE THIS,
   RECAPTCHA_KEY: '"6LemTFoUAAAAALXAe6O-HyArFBFI6AqMScBkBkid"',
-  STEEM_CONNECT_ID: '"utopian"',
+  STEEM_CONNECT_ID: '"utopian.sync"',
   LINKEDIN_CLIENT_ID: '""' // CHANGE THIS
 }
